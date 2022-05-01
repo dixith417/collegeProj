75e15392f7ea6793c6ed2a4f80b2d223967b387c
Harsh Dixit <dixith417@gmail.com>
2022-05-01 20:02:13 +0530

    comments fixed

--- Dapp/src/Components/About.js
+++ Dapp/src/Components/About.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from "react-bootstrap";
 
 const About = () => {
-  return(
-    <div className='container' style={{minHeight: "60vh"}}>
-      <div className='row'>
-        <div className='col-12'>
+  return (
+    <div className="container" style={{ minHeight: "60vh", marginTop: "3rem" }}>
+      <div className="row">
+        <div className="col-12">
           <h3>About Us</h3>
           <hr />
         </div>
       </div>
       <div className="row row-content">
         <div className="col-12 col-md-6">
-          <h2>Our History</h2>
-            <p>Describe</p>
+          <h2>Our Introduction</h2>
+          <p style={{ textAlign: "justify" }}>
+            Hello dear users, ours is a team of three passionate developers that
+            aims to provide a voting experience by using the blockchain
+            technology and integrating a discussion forum where a user can
+            comment on their candidates and ask queries. The voting Dapp uses
+            NEAR blockchain to make polls and execute the polling process.{" "}
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            Hope you enjoy the experience. Thankyou!
+          </p>
         </div>
         <div className="col-12 col-md-5">
           <Card>
-            <Card.Header className="text-white" style={{background: "#3AAFA9"}}>Facts At a Glance</Card.Header>
+            <Card.Header
+              className="text-white"
+              style={{ background: "#3AAFA9" }}
+            >
+              Facts At a Glance
+            </Card.Header>
             <Card.Body>
               <dl className="row p-1">
                 <dt className="col-6">Started</dt>
--- Dapp/src/Components/Home.js
+++ Dapp/src/Components/Home.js
@@ -1,6 +1,6 @@
 import { Tab } from "bootstrap";
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Table, Container, Button, Card } from "react-bootstrap";
 import Dashboard from "./Dashboard";
 
@@ -35,45 +35,45 @@ const Home = (props) => {
 
   return (
     <>
-    <Dashboard />
-    <Container>
-      <Table style={{ margin: "5vh" }} striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Poll Name</th>
-            <th>Go to Poll</th>
-            <th>
-              <Button onClick={deleteAllPolls}>Delete Poll</Button>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {promptList.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{item}</td>
-                <td colSpan={2}>
-                  {" "}
-                  <Button
-                    onClick={() => {
-                      if (!checkLogin()) {
-                        alert("User not logged in, please login first!");
-                        return;
-                      }
-                      props.changeCandidates(item);
-                    }}
-                  >
-                    Go to Poll
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-    </Container>
+      <Dashboard />
+      <Container>
+        <Table style={{ margin: "5vh" }} striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Poll Name</th>
+              <th>Go to Poll</th>
+              <th>
+                <Button onClick={deleteAllPolls}>Delete Poll</Button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {promptList.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item}</td>
+                  <td colSpan={2}>
+                    {" "}
+                    <Button
+                      onClick={() => {
+                        if (!checkLogin()) {
+                          alert("User not logged in, please login first!");
+                          return;
+                        }
+                        props.changeCandidates(item);
+                      }}
+                    >
+                      Go to Poll
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
     </>
   );
 };
